0bf6599422032c05cdae336ce4012497b8a719b5
Tamutoki <tamura.t0106@gmail.com>
2026-01-06 23:29:49 +0900

    a

--- img_app/img_app/src/conversion.php
+++ img_app/img_app/src/conversion.php
@@ -2,35 +2,53 @@
 <?php
 $tmp_file = $_FILES['upload']['tmp_name']; # 一時ファイル名
 $true_file = $_FILES['upload']['name']; # 本来のファイル名
-$formatold = $_POST['formatold'];
+//$formatold = $_POST['formatold'];
 $format = $_POST['format'];
 $size = $_POST['size'];
 # is_uploaded_fileメソッドで、一時的にアップロードされたファイルが本当にアップロード処理されたかの確認
 
-if (is_uploaded_file($tmp_file)) {
-    if (move_uploaded_file($tmp_file , $true_file )) {
-        echo '';
+//アップロード処理
+function imupload($tmp_file , $true_file){
+    if (is_uploaded_file($tmp_file)) {
+        if (move_uploaded_file($tmp_file , $true_file )) {
+            echo '';
+        } else {
+            echo 'ファイルをアップロードできません。<a href="?do=home" class="button">戻る</a>';
+            exit;
+        }
     } else {
-        echo 'ファイルをアップロードできません。<button><a href="?do=home">戻る</a></button>';
+        echo 'ファイルが選択されていません。<a class="button" href="?do=home">戻る</a>';
+        exit;
     }
-} else {
-    echo 'ファイルが選択されていません。<button><a href="?do=home">戻る</a></button>';
 }
-    
-$iminfo = getimagesize($true_file);
-if($iminfo == false){
-    echo "画像を認識できませんでした。";
+//情報取得
+function iminfo($true_file){
+    $iminfo = getimagesize($true_file);
+    if($iminfo == false){
+        echo '画像を認識できませんでした。<a href="?do=home" class="button">戻る</a>';
+        exit;
+    }
 }
+
+imupload($tmp_file , $true_file);
+iminfo($true_file);
 
 //前のファイル形式を取得
+// MIMEタイプを取得
+$finfo = finfo_open(FILEINFO_MIME_TYPE);
+$mime_type = finfo_file($finfo, $true_file);
+finfo_close($finfo);
 
-if($formatold == 1){
+// MIMEタイプを表示
+//echo "MIMEタイプ: " . $mime_type;
+
+if($mime_type == 'image/jpeg'){
     echo "<br>JPEG形式を";
     $im = imagecreatefromjpeg($true_file);
-}else if($formatold == 2){
+}else if($mime_type == 'image/png'){
     echo "<br>PNG形式を";
     $im = imagecreatefrompng($true_file);
-}else if($formatold == 3){
+}else if($mime_type == 'image/webp'){
     echo "<br>WEBP形式を";
     $im = imagecreatefromwebp($true_file);
 }else{
--- img_app/img_app/src/home.php
+++ img_app/img_app/src/home.php
@@ -1,16 +1,23 @@
+<?php
+$imgDir = __DIR__ . '/../img';
+
+if (is_dir($imgDir)) {
+    foreach (glob($imgDir . '/*') as $file) {
+        if (is_file($file)) {
+            unlink($file);
+        }
+    }
+}
+?>
 <h2>画像フォーマット変換</h2>
 <!-- フォーマット選択 -->
 
 <form enctype='multipart/form-data'  action='?do=conversion' method='POST'>
 <input type='hidden' name='name' value='value' />
 
-アップロード: <input type='file' name='upload' />
+アップロード: <input type='file' name='upload' required>
 <table>
-<tr><td>元の形式</td>
-<td><input type="radio" name="formatold" value=1 checked/>JPEG</td>
-<td><input type="radio" name="formatold" value=2 >PNG</td>
-<td><input type="radio" name="formatold" value=3 >WebP</td>
-</tr>
+
 <tr><td>↓</td></tr>
 <tr><td>変換したい形式</td>
 <td><input type="radio" name="format" value=1 checked/>JPEG</td>
